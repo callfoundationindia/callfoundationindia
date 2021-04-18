742654618ba5300d1e97d08d909419ab5597e9e2
Stackbit Code Editor <projects@stackbit.com>
2021-04-18 12:07:22 +0000

    main.js: changed

--- static/js/main.js
+++ static/js/main.js
@@ -29,21 +29,3 @@ Array.from(faqAccordions).forEach((faqAccordion) => {
     multiSelectable: true
   });
 });
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("masthead");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
